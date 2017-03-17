26847bf30a7d61b200dece4a8a8b0f84e749137a
jani gorse <jani.gorse@gmail.com>
2017-03-17 21:32:22 +0100

    add countdown functionality

---
+++ app/components/Controls.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Controls = React.createClass({
+    propTypes: {
+        countdownStatus: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        
+    }
+});
+
+module.exports = Controls;
